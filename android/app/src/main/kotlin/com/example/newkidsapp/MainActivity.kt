319496f1893b337fff94660320cb59f174fe0707
Giulia <giuliadanso@gmail.com>
2023-12-14 12:51:22 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/example/newkidsapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.newkidsapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/login_registration/login.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(' Welcome to Login'),
+        ),
+        body: const LoginScreen(),
+      ),
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            controller: _emailController,
+            decoration: const InputDecoration(
+              labelText: 'Email',
+              border: UnderlineInputBorder(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
